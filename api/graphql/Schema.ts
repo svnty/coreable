f416e8f4676f3f27fa0c9a57c14d486e4951af71
Jake Walklate <jakewalklate@gmail.com>
2020-04-07 09:31:30 +1000

    Delete Schema.ts

--- api/graphql/Schema.ts
+++
@@ -1,8 +0,0 @@
-import { GraphQLSchema } from "graphql";
-import { Query } from "./Query";
-import { Mutation } from './Mutation';
-
-export const Schema: GraphQLSchema = new GraphQLSchema({
-  query: Query,
-  mutation: Mutation
-});
